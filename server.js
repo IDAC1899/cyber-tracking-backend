d5ae5b25b347c4d9d3632a74f1103af3c9471acf
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-11 21:55:19 +0300

    mounted auth and threat routers in server

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const logger = require('morgan');
 
 // Routers
 const authRouter = require('./routes/authRoutes');
+const threatRouter = require('./routes/threatRoutes');
 
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
 // ROUTES
+app.use('/auth', authRouter);
+app.use('/threats', threatRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
