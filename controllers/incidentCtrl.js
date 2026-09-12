ee82812f1130ffa717d49a8ec7e5c9c5eb058222
Dana AlSaleh <d.alsaleh128@gmail.com>
2026-09-12 19:35:57 +0300

    Added show function to Incident controller

--- controllers/incidentCtrl.js
+++ controllers/incidentCtrl.js
@@ -24,8 +24,24 @@ try {
 }
 };
 
+// SHOW - get a single incident by id, populated with the assigned user's info
+// returns 200 with the incident, 404 if not found, or 500 if something goes wrong
+
+const show = async (req,res) => {
+try {
+  const incident = await Incident.findById(req.params.id).populate('assignedTo', 'username name');
+  if(!incident){
+    return res.status(404).json({ err: 'Incident not found'});
+  }
+  res.status(200).json({ incident });
+} catch (err) {
+  res.status(500).json({ err: err.message }); 
+}
+};
+
 
 module.exports = {
     create,
     index,
+    show,
 };
