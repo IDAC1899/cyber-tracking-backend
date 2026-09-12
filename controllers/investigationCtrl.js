91c982f662dafaab078fa89632907d1d81eec3ad
Munnerha <muneerha8@gmail.com>
2026-09-12 09:45:37 +0300

    Added show controller function for investigations

--- controllers/investigationCtrl.js
+++ controllers/investigationCtrl.js
@@ -20,3 +20,17 @@ const index = async (req, res) => {
   }
 };
 
+const show = async (req, res) => {
+  try {
+    const investigation = await Investigation.findById(req.params.id)
+      .populate('incident')
+      .populate('assignedTo', 'username name');
+    if (!investigation) {
+      return res.status(404).json({ err: 'Investigation not found' });
+    }
+    res.status(200).json({ investigation });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+};
+
