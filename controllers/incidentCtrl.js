11089c924f5d81fd4b0021e42719c317985dfba0
Dana AlSaleh <d.alsaleh128@gmail.com>
2026-09-12 19:27:33 +0300

    Added index function to Incident controller

--- controllers/incidentCtrl.js
+++ controllers/incidentCtrl.js
@@ -13,6 +13,19 @@ try {
 }
 };
 
+// INDEX - get all incidents, populated with the assigned user's info
+// returns 200 with the list, or 500 if something goes wrong
+const index = async (req,res) => {
+try {
+  const incidents = await Incident.find({}).populate('assignedTo', 'username name');
+  res.status(200).json({ incidents }); 
+} catch (err) {
+  res.status(500).json({ err: err.message }); 
+}
+};
+
+
 module.exports = {
     create,
+    index,
 };
