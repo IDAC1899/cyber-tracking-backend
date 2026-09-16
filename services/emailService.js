0fea4c57d6bff4287fce30ecd7b4f936868a0108
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-16 11:05:42 +0300

    added Nodemailer email service following GA's intro-to-nodemailer pattern

---
+++ services/emailService.js
@@ -0,0 +1,22 @@
+// services/emailService.js
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_APP_PASSWORD,
+  },
+});
+
+const sendEmail = async ({ to, subject, text }) => {
+  await transporter.sendMail({
+    from: `CyberTrack <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text,
+  });
+};
+
+module.exports = sendEmail;
